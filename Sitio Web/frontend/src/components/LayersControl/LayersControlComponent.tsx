bd12148324cdd7786542a9be2b2523b3f99727c6
Sergio Saavedra <sergio.saavedrajr@gmail.com>
2024-11-16 10:17:51 -0300

    Layers de comunas

--- Sitio Web/frontend/src/components/LayersControl/LayersControlComponent.tsx
+++ Sitio Web/frontend/src/components/LayersControl/LayersControlComponent.tsx
@@ -1,70 +1,120 @@
 import * as turf from "@turf/turf";
 import { LayerGroup, LayersControl, Marker, Polygon, Popup } from "react-leaflet";
+import { useState } from "react";
 import { useUserStore } from "../../state/State";
 import comunasData from "./comunas.json";
 import { MarcadorIcon } from "../MarcadorIcon/MarcadorIcon";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from "@mui/material";
+import { toast } from 'sonner';
 
 export const LayersControlComponent = () => {
     const { nodes, showMuseos, showMonumentos, showIglesias, showParques } = useUserStore();
+    const [openDialog, setOpenDialog] = useState(false);
+    const [dialogContent, setDialogContent] = useState({ title: "", description: "" });
+
+    const fetchMuseoData = async (nombreMuseo: any) => {
+        const loadingToast = toast("Cargando...", { duration: Infinity });
+
+        try {
+            const response = await fetch(`http://127.0.0.1:5000//buscar-museo?nombre_museo=${encodeURIComponent(nombreMuseo)}`);
+            const data = await response.json();
+
+            // Asignamos los datos recibidos al contenido del diálogo
+            setDialogContent({
+                title: data.nombre || nombreMuseo,
+                description: data.descripcion || "Información no disponible",
+            });
+        } catch (error) {
+            console.error("Error al buscar la información del museo:", error);
+            setDialogContent({
+                title: nombreMuseo,
+                description: "Error al cargar la información del museo.",
+            });
+        } finally {
+            toast.dismiss(loadingToast);  // Cerrar el toast de "Cargando..."
+            setOpenDialog(true);          // Abrir el modal con la información
+        }
+    };
+
+    const handleOpenDialog = (nombreMuseo: any) => {
+        fetchMuseoData(nombreMuseo);
+    };
 
     return (
-        <LayersControl position="topright">
-            {comunasData.features.map((feature: any, comunaIndex: any) => {
-                // Crear el polígono usando las coordenadas de la comuna
-                const polygonCoords = feature.geometry.coordinates.map((polygon: any) =>
-                    polygon.map((coord: any) => [coord[1], coord[0]])
-                );
+        <>
+            <LayersControl position="topright">
+                {comunasData.features.map((feature, comunaIndex) => {
+                    const polygonCoords = feature.geometry.coordinates.map((polygon) =>
+                        polygon.map((coord) => [coord[1], coord[0]])
+                    );
 
-                const turfPolygon = turf.polygon(polygonCoords);
+                    const turfPolygon = turf.polygon(polygonCoords);
 
-                const pointsInPolygon = [
-                    ...(showMuseos ? nodes.museos : []),
-                    ...(showMonumentos ? nodes.monumentos : []),
-                    ...(showIglesias ? nodes.iglesias : []),
-                    ...(showParques ? nodes.parques : []),
-                ].filter((point: any) => {
-                    const turfPoint = turf.point([point.lat, point.lon]);
-                    const isInPolygon = turf.booleanPointInPolygon(turfPoint, turfPolygon);
-                    console.log(`Punto: [${point.lon}, ${point.lat}], En polígono: ${isInPolygon}`);
-                    return isInPolygon;
-                });
-                
+                    const pointsInPolygon = [
+                        ...(showMuseos ? nodes.museos : []),
+                        ...(showMonumentos ? nodes.monumentos : []),
+                        ...(showIglesias ? nodes.iglesias : []),
+                        ...(showParques ? nodes.parques : []),
+                    ].filter((point) => {
+                        const turfPoint = turf.point([point.lat, point.lon]);
+                        const isInPolygon = turf.booleanPointInPolygon(turfPoint, turfPolygon);
+                        return isInPolygon;
+                    });
 
-                const icon = MarcadorIcon({ color: "red" });
+                    const icon = MarcadorIcon({ color: "red" });
 
-                return (
-                    <LayersControl.Overlay key={comunaIndex} name={feature.properties.Comuna}>
-                        <LayerGroup>
-                            {/* Dibujar el perímetro de la comuna */}
-                            {polygonCoords.map((polygon: any, polygonIndex: any) => (
-                                <Polygon
-                                    key={polygonIndex}
-                                    positions={polygon}
-                                    pathOptions={{ color: "blue" }}
-                                >
-                                    <Popup>
-                                        <strong>{feature.properties.Comuna}</strong>
-                                        <br />
-                                        Región: {feature.properties.Region}
-                                        <br />
-                                        Provincia: {feature.properties.Provincia}
-                                    </Popup>
-                                </Polygon>
-                            ))}
+                    return (
+                        <LayersControl.Overlay key={comunaIndex} name={feature.properties.Comuna}>
+                            <LayerGroup>
+                                {polygonCoords.map((polygon: any, polygonIndex: any) => (
+                                    <Polygon
+                                        key={polygonIndex}
+                                        positions={polygon}
+                                        pathOptions={{ color: "blue" }}
+                                    >
+                                        <Popup>
+                                            <strong>{feature.properties.Comuna}</strong>
+                                            <br />
+                                            Región: {feature.properties.Region}
+                                            <br />
+                                            Provincia: {feature.properties.Provincia}
+                                        </Popup>
+                                    </Polygon>
+                                ))}
 
-                            {pointsInPolygon.map((point, pointIndex) => (
-                                
-                                <Marker key={pointIndex} position={[point.lat, point.lon]} icon={icon}>
-                                    <Popup>
-                                        {point.type.charAt(0).toUpperCase() + point.type.slice(1)}
-                                        dentro de {feature.properties.Comuna}
-                                    </Popup>
-                                </Marker>
-                            ))}
-                        </LayerGroup>
-                    </LayersControl.Overlay>
-                );
-            })}
-        </LayersControl>
+                                {pointsInPolygon.map((point, pointIndex) => (
+                                    <Marker key={pointIndex} position={[point.lat, point.lon]} icon={icon}>
+                                        <Popup>
+                                            {point.tags?.name || "Sin nombre"}
+                                            <br />
+                                            <Button
+                                                variant="text"
+                                                color="primary"
+                                                onClick={() => handleOpenDialog(point.tags?.name || "Sin nombre")}
+                                            >
+                                                Ver más
+                                            </Button>
+                                        </Popup>
+                                    </Marker>
+                                ))}
+                            </LayerGroup>
+                        </LayersControl.Overlay>
+                    );
+                })}
+            </LayersControl>
+
+            {/* Dialog para mostrar información del museo */}
+            <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+                <DialogTitle>{dialogContent.title}</DialogTitle>
+                <DialogContent>
+                    <Typography>{dialogContent.description}</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpenDialog(false)} color="primary">
+                        Cerrar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
